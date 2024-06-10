22d5940014fb1fd8be02ba17dd325e8fa847a37e
Diellza Ajeti <diellzaajeti13@gmail.com>
2024-06-10 12:55:50 +0200

    Contact Page (Dark and Light Mode)

---
+++ js/contact-script.js
@@ -0,0 +1,19 @@
+const inputs = document.querySelectorAll(".contact-input");
+const toggleBtn = document.querySelector(".theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+})
+
+inputs.forEach(ipt => {
+    ipt.addEventListener("focus", () =>{
+        ipt.parentNode.classList.add("focus");
+        ipt.parentNode.classList.add("not-empty");
+    });
+    ipt.addEventListener("blur", () =>{
+        if (ipt.value == ""){
+            ipt.parentNode.classList.remove("not-empty");
+        }
+        ipt.parentNode.classList.remove("focus");
+    });
+})
